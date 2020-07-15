e655c7cf7ef11ebf3b55c012979ac707411989fc
janestreeter <jane.america.streeter@gmail.com>
2020-07-15 15:04:57 -0500

    im on lecture 129

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
         <div class="row">
             <div class="col-xs-12">
                <app-Quote>
-                   <h2>{{ quoteTitle }}</h2>
-               <p>A wonderful Quote</p>
+                   <h2 slot="title">{{ quoteTitle }}</h2>
+                   <p slot="content">A wonderful Quote</p>
                </app-Quote>      
             </div>
         </div>
