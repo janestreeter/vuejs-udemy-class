8718f319dc275d5d75a8aa221d3a2200fd4cebe1
janestreeter <jane.america.streeter@gmail.com>
2020-08-07 13:47:51 -0500

    im moving on to section 14

--- src/App.vue
+++ src/App.vue
@@ -3,32 +3,39 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
-                <p>{{ text | toUppercase | to-lowercase }}</p>
+                <p>{{ Text | toUpperCase | to-lowercase }}</p>
                 <hr>
+                <button @click="fruits.push('Berries')">Add New Item</button>
                 <input v-model="filterText">
                 <ul>
                     <li v-for="fruit in filteredFruits" v-bind:key="fruit">{{ fruit }}</li>
                 </ul>
+                <hr>
+                <app-list></app-list>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import List from './List.vue';
+    import { fruitMixin } from './fruitMixin';
+
     export default {
+        mixins: [fruitMixin],
        data() {
            return {
-                text: 'Hello there!',
-                fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
-                filter: ''   
+                Text: 'Hello there!' 
+
            }
        },
-       computed: {
-           filteredFruits() {
-               return this.fruits.filter((element) => {
-                   return element.match(this.filterText);
-               });
+       filters: {
+           toUpperCase(value) {
+               return value.toUpperCase();
            }
+       },
+       components: {
+           appList: List 
        }
    }
 </script>
