b18387a3f65b02e18b6c701e15dd6129f07c0f51
janestreeter <jane.america.streeter@gmail.com>
2020-08-07 21:54:08 -0500

    im on lecture 207

--- src/App.vue
+++ src/App.vue
@@ -2,43 +2,157 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Filters & Mixins</h1>
-                <p>{{ Text | toUpperCase | to-lowercase }}</p>
+                <h1>Animations</h1>
                 <hr>
-                <button @click="fruits.push('Berries')">Add New Item</button>
-                <input v-model="filterText">
-                <ul>
-                    <li v-for="fruit in filteredFruits" v-bind:key="fruit">{{ fruit }}</li>
-                </ul>
+                 <select v-model="alertAnimation" v-bind:key="alertAnimation" class="form-control">
+                     <option value="fade">Fade</option>
+                     <option value="slide">Slide</option>
+                 </select>
+                <br><br>
+                <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+                <br><br>
+                <transition :name="alertAnimation">
+                    <div class="alert alert-info" v-show="show">This is some Info</div>
+                </transition>
+                    <transition name="slide" type="animation" appear>
+                    <div class="alert alert-info" v-if="show">This is some Info</div>
+                </transition>
+                 <transition 
+                        enter-active-class="animated bounce"
+                        leave-active-class="animated shake"
+                >     
+                    <div class="alert alert-info" v-if="show">This is some Info</div>
+                        </transition>   
+                       <transition :name="alertAnimation" mode="out-in">
+                    <div class="alert alert-info" v-if="show" key="info">This is some Info</div>
+                    <div class="alert alert-warning" v-else key="warning">This is some Warning</div>
+                </transition>
                 <hr>
-                <app-list></app-list>
+                <button class="btn btn-primary" @click="load = !load">Load / Remove Element</button>
+                <br><br>
+                <transition
+                    @before-enter="beforeEnter"
+                    @enter="enter"
+                    @after-enter="afterEnter"
+                    enter-cancelled="enterCancelled"
+                    
+                    @before-leave="beforeLeave"
+                    @leave="leave"
+                    @after-leave="afterLeave"
+                    @leave-cancelled="leaveCancelled"
+                    :css="false">
+                    <div style="width: 100px; height: 100px; background-color: lightgreen" v-if="load"></div>
+                </transition>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import List from './List.vue';
-    import { fruitMixin } from './fruitMixin';
-
     export default {
-        mixins: [fruitMixin],
-       data() {
-           return {
-                Text: 'Hello there!' 
-
-           }
-       },
-       filters: {
-           toUpperCase(value) {
-               return value.toUpperCase();
-           }
-       },
-       components: {
-           appList: List 
-       }
-   }
+        data() {
+            return {
+                show: false,
+                load: true,
+                alertAnimation: 'fade',
+                elementWidth: 100
+            }
+        },
+        methods: {
+            beforeEnter(el) {
+                console.log('beforeEnter');
+                this.elementWidth = 100;
+                el.style.width = this.elementWidth + 'px';
+             },
+            enter(el, done) {
+                console.log('enter');
+                let round = 1;
+                const interval = setInterval(() => {
+                    el.style.width = (thiselementwidth + round * 10) + 'px';
+                    round++;
+                    if (round > 20) {
+                        clearInterval(interval);
+                        done();
+                    }
+                }, 20);
+            },
+            afterEnter(el) {
+                console.log('afterEnter');
+            },
+            enterCancelled(el) {
+                console.log('enterCancelled');
+            },
+            beforeLeave(el) {
+                console.log('beforeLeave');
+                      this.elementWidth = 100;
+                el.style.width = this.elementWidth + 'px';
+            },
+            leave(el, done) {
+                console.log('leave');
+                done();
+            },
+            afterLeave(el) {
+                console.log('afterLeave');
+            },
+            leaveCancelled(el) {
+                console.log('leaveCancelled');
+            }
+        }
+    }
 </script>
 
 <style>
+    .fade-enter {
+        opacity: 0;
+    }
+
+    .fade-enter-active {
+        transition: opacity 1s;
+    }
+
+    .fade-leave {
+        /* opacity: 1; */
+    }
+
+    .fade-leave-active {
+        transition: opacity 1s;
+        opacity: 0;
+    }
+
+    .slide-enter {
+        opacity: 0;
+        /* transform: translateY(20px); */
+    }
+
+    .slide-enter-active {
+        animation: slide-in 1s ease-out forwards;
+        transition: opacity .5s;
+    }
+
+    .slide-leave {
+
+    }
+
+    .slide-leave-active {
+        animation: slide-out 1s ease-out forwards;
+        transition: opacity 1s;
+        opacity: 0;
+    }
+
+    @keyframes slide-in {
+        from {
+            transform: translateY(20px);
+        }
+        to {
+            transform: translateY(0);
+        }
+    }
+       @keyframes slide-out {
+        from {
+            transform: translateY(0);
+        }
+        to {
+            transform: translateY(20px);
+        }
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.filter('to-lowercase', function(value) {
-    return value.toLowerCase();
-});
-
-Vue.mixin({
-    created() {
-        console.log('Global Mixin - Created Hook');
-    }
-});
-
 new Vue({
   el: '#container',
   render: h => h(App)
